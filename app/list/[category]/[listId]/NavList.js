ea33f0a82684acd2fb8f9b875f6b68528e0c38d9
soheeyeo <sohee407@gmail.com>
2023-12-15 23:47:08 +0900

    :lipstick: Update chocolate data

--- app/list/[category]/[listId]/NavList.js
+++ app/list/[category]/[listId]/NavList.js
@@ -1,27 +1,32 @@
 import Link from "next/link";
+import { useState } from "react";
 
-export default function NavList({ styles }) {
+export default function NavList({ styles, category }) {
+    const [selecte, setSelected] = useState("");
+
     return (
         <div className={styles.nav_container}>
             <nav className={styles.nav_li}>
                 <Link
-                    href={"/list/price/1"}
+                    href={`/list/${category}/1`}
                     className={`${styles.nav_btn} ${styles.active}`}
-                >
-                    ~1만원
-                </Link>
-                <Link href={"/list/price/2"} className={styles.nav_btn}>
-                    1만원대
-                </Link>
-                <Link href={"/list/price/3"} className={styles.nav_btn}>
-                    2만원대
-                </Link>
-                <Link href={"/list/price/4"} className={styles.nav_btn}>
-                    3만원대
-                </Link>
-                <Link href={"/list/price/5"} className={styles.nav_btn}>
-                    4만원~
-                </Link>
+                ></Link>
+                <Link
+                    href={`/list/${category}/2`}
+                    className={styles.nav_btn}
+                ></Link>
+                <Link
+                    href={`/list/${category}/3`}
+                    className={styles.nav_btn}
+                ></Link>
+                <Link
+                    href={`/list/${category}/4`}
+                    className={styles.nav_btn}
+                ></Link>
+                <Link
+                    href={`/list/${category}/5`}
+                    className={styles.nav_btn}
+                ></Link>
             </nav>
         </div>
     );
