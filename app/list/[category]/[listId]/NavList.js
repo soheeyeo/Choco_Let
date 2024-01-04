4507faf63a2f4807edb5316d80203e0d0ebf3ed6
soheeyeo <sohee407@gmail.com>
2024-01-04 23:53:21 +0900

    :sparkles: Add tab function to NavList

--- app/list/[category]/[listId]/NavList.js
+++ app/list/[category]/[listId]/NavList.js
@@ -1,32 +1,42 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useParams } from "next/navigation";
+import {
+    priceCategory,
+    countryCategory,
+    typeCategory,
+    flavorCategory,
+} from "@/constants/constants";
 
 export default function NavList({ styles, category }) {
-    const [selecte, setSelected] = useState("");
+    const params = useParams();
+
+    let list_item_category = [];
+
+    if (category === "price") {
+        list_item_category = priceCategory;
+    } else if (category === "country") {
+        list_item_category = countryCategory;
+    } else if (category === "type") {
+        list_item_category = typeCategory;
+    } else {
+        list_item_category = flavorCategory;
+    }
 
     return (
         <div className={styles.nav_container}>
             <nav className={styles.nav_li}>
-                <Link
-                    href={`/list/${category}/1`}
-                    className={`${styles.nav_btn} ${styles.active}`}
-                ></Link>
-                <Link
-                    href={`/list/${category}/2`}
-                    className={styles.nav_btn}
-                ></Link>
-                <Link
-                    href={`/list/${category}/3`}
-                    className={styles.nav_btn}
-                ></Link>
-                <Link
-                    href={`/list/${category}/4`}
-                    className={styles.nav_btn}
-                ></Link>
-                <Link
-                    href={`/list/${category}/5`}
-                    className={styles.nav_btn}
-                ></Link>
+                {list_item_category.map((a, i) => {
+                    return (
+                        <Link
+                            href={`/list/${category}/${i + 1}`}
+                            className={`${styles.nav_btn} ${
+                                params.listId == i + 1 ? styles.active : ""
+                            }`}
+                        >
+                            {a}
+                        </Link>
+                    );
+                })}
             </nav>
         </div>
     );
--- constants/constants.js
+++ constants/constants.js
@@ -16,3 +16,17 @@ export const categories = [
         link: "/list/flavor/1",
     },
 ];
+
+export const priceCategory = [
+    "~1만 원",
+    "1만 원대",
+    "2~3만 원대",
+    "4~5만 원대",
+    "6만 원 ~",
+];
+
+export const countryCategory = ["벨기에", "독일", "미국", "대한민국", "그 외"];
+
+export const typeCategory = ["밀크", "다크", "화이트", "프랄린", "그 외"];
+
+export const flavorCategory = ["아몬드", "헤이즐넛", "카라멜", "딸기", "그 외"];
