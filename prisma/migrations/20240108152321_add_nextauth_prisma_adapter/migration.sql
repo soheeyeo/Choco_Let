e0c5101df14b8d4376c4e709f61a6ea39dfa383b
soheeyeo <sohee407@gmail.com>
2024-01-09 22:28:13 +0900

    :memo: Modify prisma schema

---
+++ prisma/migrations/20240108152321_add_nextauth_prisma_adapter/migration.sql
@@ -0,0 +1,83 @@
+/*
+  Warnings:
+
+  - The primary key for the `user` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `userId` on the `user` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE `like` DROP FOREIGN KEY `Like_userId_fkey`;
+
+-- DropIndex
+DROP INDEX `User_userId_key` ON `user`;
+
+-- AlterTable
+ALTER TABLE `like` MODIFY `userId` VARCHAR(191) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `user` DROP PRIMARY KEY,
+    DROP COLUMN `userId`,
+    ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `emailVerified` DATETIME(3) NULL,
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL,
+    MODIFY `id` VARCHAR(191) NOT NULL,
+    MODIFY `password` VARCHAR(191) NULL,
+    MODIFY `name` VARCHAR(191) NULL,
+    MODIFY `email` VARCHAR(191) NULL,
+    ADD PRIMARY KEY (`id`);
+
+-- CreateTable
+CREATE TABLE `Account` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `providerType` VARCHAR(191) NOT NULL,
+    `providerId` VARCHAR(191) NOT NULL,
+    `providerAccountId` VARCHAR(191) NOT NULL,
+    `refreshToken` VARCHAR(191) NULL,
+    `accessToken` VARCHAR(191) NULL,
+    `accessTokenExpires` DATETIME(3) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Account_providerId_providerAccountId_key`(`providerId`, `providerAccountId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+    `sessionToken` VARCHAR(191) NOT NULL,
+    `accessToken` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Session_sessionToken_key`(`sessionToken`),
+    UNIQUE INDEX `Session_accessToken_key`(`accessToken`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `VerificationRequest` (
+    `id` VARCHAR(191) NOT NULL,
+    `identifier` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `VerificationRequest_token_key`(`token`),
+    UNIQUE INDEX `VerificationRequest_identifier_token_key`(`identifier`, `token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Like` ADD CONSTRAINT `Like_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
