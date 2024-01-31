c27fb6eccbadd54a03217257ebdcd0fa0c513154
soheeyeo <sohee407@gmail.com>
2024-01-31 11:56:41 +0900

    :sparkles: Add toggle menu function to header

--- app/components/common/Header.js
+++ app/components/common/Header.js
@@ -4,11 +4,23 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { categories } from "@/constants/constants";
 import LogoutBtn from "../button/LogoutBtn.js";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Header() {
     const session = useSession();
+
+    const menuRef = useRef(null);
     const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        function handleOutside(e) {
+            if (menuRef.current && !menuRef.current.contains(e.target)) {
+                setActive(false);
+            }
+        }
+        document.addEventListener("mousedown", handleOutside);
+        return () => document.removeEventListener("mousedown", handleOutside);
+    }, [menuRef]);
 
     const handleMenu = () => {
         setActive(!active);
@@ -22,15 +34,18 @@ export default function Header() {
                         <img src="/assets/logo.png" className="logo_img" />
                     </Link>
                 </h1>
-                <ul className={`link_li ${active ? "active" : ""}`}>
+                <ul
+                    ref={menuRef}
+                    className={`link_li ${active ? "active" : ""}`}
+                >
                     {categories.map((a, i) => {
                         return (
-                            <li className="link" key={i}>
+                            <li className="link" key={i} onClick={handleMenu}>
                                 <Link href={a.link}>{a.category}</Link>
                             </li>
                         );
                     })}
-                    <li className="link">
+                    <li className="link" onClick={handleMenu}>
                         <Link href="/test">추천 테스트</Link>
                     </li>
                 </ul>
