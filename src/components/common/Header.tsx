96c660bb64f8d1718eb242eee81386f77a92da0f
soheeyeo <sohee407@gmail.com>
2025-01-10 01:11:40 +0900

    ♻️ Convert to tsx and rename function

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -8,21 +8,28 @@ import { useState, useEffect, useRef } from "react";
 
 export default function Header() {
     const session = useSession();
-
-    const menuRef = useRef(null);
+    const menuRef = useRef<HTMLUListElement | null>(null);
     const [active, setActive] = useState(false);
 
     useEffect(() => {
-        function handleOutside(e) {
-            if (menuRef.current && !menuRef.current.contains(e.target)) {
+        // ref 바깥 영역 클릭 시 active 상태 false로 변경
+        function handleClickOutside(e: MouseEvent) {
+            if (
+                menuRef.current &&
+                !menuRef.current.contains(e.target as Node)
+            ) {
                 setActive(false);
             }
         }
-        document.addEventListener("mousedown", handleOutside);
-        return () => document.removeEventListener("mousedown", handleOutside);
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () =>
+            document.removeEventListener("mousedown", handleClickOutside);
     }, [menuRef]);
 
-    const handleMenu = () => {
+    // 메뉴 버튼 클릭 시 active 상태 변경
+    const handleClickCategory = () => {
         setActive(!active);
     };
 
@@ -38,20 +45,29 @@ export default function Header() {
                     ref={menuRef}
                     className={`link_li ${active ? "active" : ""}`}
                 >
-                    {categories.map((a, i) => {
+                    {categories.map((category, i) => {
                         return (
-                            <li className="link" key={i} onClick={handleMenu}>
-                                <Link href={a.link}>{a.category}</Link>
+                            <li
+                                className="link"
+                                key={i}
+                                onClick={handleClickCategory}
+                            >
+                                <Link href={category.link}>
+                                    {category.category}
+                                </Link>
                             </li>
                         );
                     })}
-                    <li className="link" onClick={handleMenu}>
+                    <li className="link" onClick={handleClickCategory}>
                         <Link href="/test">추천 테스트</Link>
                     </li>
                 </ul>
                 {session.data ? (
                     <div className="account_li">
-                        <button className="menu_btn" onClick={handleMenu}>
+                        <button
+                            className="menu_btn"
+                            onClick={handleClickCategory}
+                        >
                             메뉴
                         </button>
                         <Link href="/like" className="like_btn">
