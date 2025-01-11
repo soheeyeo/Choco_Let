3e33c90e0b8b3e3c400116c4086fc0cc4643065a
soheeyeo <sohee407@gmail.com>
2025-01-11 23:44:38 +0900

    ♻️ Migrate detail, list and result components to TypeScript

--- src/app/api/result/route.tsx
+++ src/app/api/result/route.tsx
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/prisma";
 
-export async function POST(req) {
+export async function POST(req: Request) {
     const name = await req.json();
 
     const result = await prisma.chocolate.findUnique({
