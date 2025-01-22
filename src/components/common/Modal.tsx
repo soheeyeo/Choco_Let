5cd46661cae8ca687a801649b0867a49082a842b
soheeyeo <sohee407@gmail.com>
2025-01-22 15:38:47 +0900

    ♻️ Add open/close animation to Modal component

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface ModalProps {
     setIsOpen: (isOpen: boolean) => void;
@@ -8,29 +9,44 @@ interface ModalProps {
 
 export default function Modal({ setIsOpen }: ModalProps) {
     const router = useRouter();
+    const [isAnimating, setIsAnimating] = useState<boolean>(false);
+    const modalAnimationTime = 300;
 
-    const handleOnClick = () => {
-        setIsOpen(false);
+    // 모달 닫기 버튼 클릭 시 애니메이션 적용
+    const handleModalClose = () => {
+        setIsAnimating(true);
+
+        setTimeout(() => {
+            setIsOpen(false);
+            setIsAnimating(false);
+        }, modalAnimationTime);
     };
 
-    const handleRouter = () => {
+    const handleModalConfirm = () => {
+        // 현재 위치 storage에 저장 후 로그인 페이지 이동
         const storage = globalThis?.sessionStorage;
         storage.setItem("prevPath", globalThis.location.pathname);
         router.push("/login");
     };
 
     return (
-        <div className="modal">
+        <div className={`modal ${isAnimating ? "close" : ""}`}>
             <div className="modal_container">
                 <div className="modal_content">
                     <p>로그인이 필요한 서비스입니다.</p>
                     <p>로그인하시겠습니까?</p>
                 </div>
                 <div className="modal_btn_container">
-                    <button onClick={handleOnClick} className="modal_btn_l">
+                    <button
+                        onClick={handleModalClose}
+                        className="modal_cancel_btn"
+                    >
                         취소
                     </button>
-                    <button onClick={handleRouter} className="modal_btn_r">
+                    <button
+                        onClick={handleModalConfirm}
+                        className="modal_confirm_btn"
+                    >
                         확인
                     </button>
                 </div>
