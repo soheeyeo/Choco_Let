2940fb9bdc5ecb59b865155cfde10b7ed723a257
soheeyeo <sohee407@gmail.com>
2025-02-20 11:11:11 +0900

    ♻️ Refactor chocolate fetching logic and Exception handling

--- src/app/api/list/[...param]/route.tsx
+++ src/app/api/list/[...param]/route.tsx
@@ -1,4 +1,6 @@
+"use server";
 import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
 interface RouteParams {
     params: {
@@ -6,280 +8,177 @@ interface RouteParams {
     };
 }
 
-export async function GET(req: Request, { params }: RouteParams) {
-    const param = params.param;
-    if (param[0] === "price") {
-        if (param[1] === "1") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    price: {
-                        lte: 10000,
-                    },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "2") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    price: {
+export async function GET(req: NextRequest, { params }: RouteParams) {
+    try {
+        // params.param 값이 잘못된 경우 에러 반환
+        if (!params.param || params.param.length < 2) {
+            return NextResponse.json(
+                { error: "잘못된 요청 형식입니다." },
+                { status: 400 }
+            );
+        }
+
+        // 파라미터를 category와 listId로 분리
+        const [category, listId] = params.param;
+        const listIndex = Number(listId) - 1;
+
+        let result;
+
+        switch (category) {
+            // category가 price인 경우
+            case "price":
+                const priceList = [
+                    { lte: 10000 },
+                    {
                         gte: 10000,
                         lt: 20000,
                     },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "3") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    price: {
+                    {
                         gte: 20000,
                         lt: 40000,
                     },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "4") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    price: {
+                    {
                         gte: 40000,
                         lt: 60000,
                     },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "5") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    price: {
+                    {
                         gte: 60000,
                     },
-                },
-            });
-            return Response.json(result);
+                ];
+
+                // 유효한 인덱스인 경우 해당 가격 조건으로 초콜릿 데이터 조회
+                if (listIndex >= 0 && listIndex < priceList.length) {
+                    result = await prisma.chocolate.findMany({
+                        where: {
+                            price: priceList[listIndex],
+                        },
+                    });
+                }
+                break;
+
+            // category가 country인 경우
+            case "country":
+                const countryList = [
+                    ["벨기에", "이탈리아"],
+                    ["독일", "스위스"],
+                    ["미국", "영국"],
+                    "대한민국",
+                    [
+                        "벨기에",
+                        "이탈리아",
+                        "독일",
+                        "스위스",
+                        "미국",
+                        "영국",
+                        "대한민국",
+                    ],
+                ];
+
+                // 유효한 인덱스인 경우 해당 국가명 조건으로 초콜릿 데이터 조회
+                if (listIndex >= 0 && listIndex < countryList.length) {
+                    result = await prisma.chocolate.findMany({
+                        where: {
+                            country: Array.isArray(countryList[listIndex])
+                                ? listId === "5"
+                                    ? {
+                                          notIn: countryList[listIndex],
+                                      }
+                                    : { in: countryList[listIndex] }
+                                : countryList[listIndex],
+                        },
+                    });
+                }
+                break;
+
+            // category가 type인 경우
+            case "type":
+                const typeList = ["밀크", "다크", "화이트", "프랄린"];
+
+                // 유효한 인덱스인 경우 해당 종류 조건으로 초콜릿 데이터 조회
+                if (listIndex >= 0 && listIndex <= typeList.length) {
+                    result = await prisma.chocolate.findMany({
+                        where: {
+                            type:
+                                listId === "5"
+                                    ? { notIn: typeList }
+                                    : typeList[listIndex],
+                        },
+                    });
+                }
+                break;
+
+            // category가 flavor인 경우
+            case "flavor":
+                const flavorList = [
+                    [
+                        "아몬드",
+                        "헤이즐넛",
+                        "마카다미아",
+                        "피넛",
+                        "피스타치오",
+                        "견과",
+                    ],
+                    ["카라멜", "토피넛", "누가"],
+                    [
+                        "딸기",
+                        "배",
+                        "요거트",
+                        "비스킷",
+                        "콘플레이크",
+                        "쿠키",
+                        "오레오",
+                    ],
+                    "믹스",
+                    [
+                        "아몬드",
+                        "헤이즐넛",
+                        "마카다미아",
+                        "피넛",
+                        "피스타치오",
+                        "견과",
+                        "카라멜",
+                        "토피넛",
+                        "누가",
+                        "딸기",
+                        "배",
+                        "요거트",
+                        "비스킷",
+                        "콘플레이크",
+                        "쿠키",
+                        "오레오",
+                        "믹스",
+                    ],
+                ];
+
+                // 유효한 인덱스인 경우 해당 맛 조건으로 초콜릿 데이터 조회
+                if (listIndex >= 0 && listIndex < flavorList.length) {
+                    result = await prisma.chocolate.findMany({
+                        where: {
+                            flavor: Array.isArray(flavorList[listIndex])
+                                ? listId === "5"
+                                    ? {
+                                          notIn: flavorList[listIndex],
+                                      }
+                                    : { in: flavorList[listIndex] }
+                                : flavorList[listIndex],
+                        },
+                    });
+                }
+                break;
         }
-    } else if (param[0] === "country") {
-        if (param[1] === "1") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    country: {
-                        in: ["벨기에", "이탈리아"],
-                    },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "2") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    country: {
-                        in: ["독일", "스위스"],
-                    },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "3") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    country: {
-                        in: ["미국", "영국"],
-                    },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "4") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    country: "대한민국",
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "5") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    country: {
-                        notIn: [
-                            "벨기에",
-                            "이탈리아",
-                            "독일",
-                            "스위스",
-                            "미국",
-                            "영국",
-                            "대한민국",
-                        ],
-                    },
-                },
-            });
-            return Response.json(result);
+
+        // result가 없는 경우 예외처리
+        if (!result) {
+            return NextResponse.json(
+                { error: "잘못된 값입니다." },
+                { status: 400 }
+            );
         }
-    } else if (param[0] === "type") {
-        if (param[1] === "1") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    type: "밀크",
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "2") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    type: "다크",
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "3") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    type: "화이트",
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "4") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    type: "프랄린",
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "5") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    type: {
-                        notIn: ["밀크", "다크", "화이트", "프랄린"],
-                    },
-                },
-            });
-            return Response.json(result);
-        }
-    } else if (param[0] === "country") {
-        if (param[1] === "1") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    country: {
-                        in: ["벨기에", "이탈리아"],
-                    },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "2") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    country: {
-                        in: ["독일", "스위스"],
-                    },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "3") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    country: {
-                        in: ["미국", "영국"],
-                    },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "4") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    country: "대한민국",
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "5") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    country: {
-                        notIn: [
-                            "벨기에",
-                            "이탈리아",
-                            "독일",
-                            "스위스",
-                            "미국",
-                            "영국",
-                            "대한민국",
-                        ],
-                    },
-                },
-            });
-            return Response.json(result);
-        }
-    } else if (param[0] === "flavor") {
-        if (param[1] === "1") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    flavor: {
-                        in: [
-                            "아몬드",
-                            "헤이즐넛",
-                            "마카다미아",
-                            "피넛",
-                            "피스타치오",
-                            "견과",
-                        ],
-                    },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "2") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    flavor: {
-                        in: ["카라멜", "토피넛", "누가"],
-                    },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "3") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    flavor: {
-                        in: [
-                            "딸기",
-                            "배",
-                            "요거트",
-                            "비스킷",
-                            "콘플레이크",
-                            "쿠키",
-                            "오레오",
-                        ],
-                    },
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "4") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    flavor: "믹스",
-                },
-            });
-            return Response.json(result);
-        } else if (param[1] === "5") {
-            const result = await prisma.chocolate.findMany({
-                where: {
-                    flavor: {
-                        notIn: [
-                            "아몬드",
-                            "헤이즐넛",
-                            "마카다미아",
-                            "피넛",
-                            "피스타치오",
-                            "견과",
-                            "카라멜",
-                            "토피넛",
-                            "누가",
-                            "딸기",
-                            "배",
-                            "요거트",
-                            "비스킷",
-                            "콘플레이크",
-                            "쿠키",
-                            "오레오",
-                            "믹스",
-                        ],
-                    },
-                },
-            });
-            return Response.json(result);
-        }
+        return NextResponse.json(result, { status: 200 });
+    } catch (error) {
+        console.error("데이터 조회 중 오류가 발생했습니다.", error);
+        throw new Error(
+            "데이터 조회 중 문제가 발생했습니다. 잠시 후 다시 시도해주세요."
+        );
     }
 }
