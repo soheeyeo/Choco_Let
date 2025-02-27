b050bcd6f9ca0a73356fb2d89106b36bfef5a339
soheeyeo <sohee407@gmail.com>
2025-02-27 11:59:42 +0900

    ♻️ Use fetchData for data fetching and optimize sorting with useMemo

--- src/app/list/[category]/[listId]/Content.tsx
+++ src/app/list/[category]/[listId]/Content.tsx
@@ -1,7 +1,7 @@
 "use client";
 import DropdownBtn from "./DropdownBtn";
 import ItemCard from "./ItemCard";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import useGetLike from "@/hooks/useGetLike";
 import { Chocolate } from "@prisma/client";
 
@@ -12,38 +12,35 @@ interface ContentProps {
 
 export default function Content({ itemList, styles }: ContentProps) {
     const { likedItemList } = useGetLike();
-
     const [selected, setSelected] = useState<string>("추천순");
-
-    const data = itemList;
 
     useEffect(() => {
         sessionStorage?.removeItem("prevPath");
     }, []);
 
-    function handleSortItems(selected: string) {
-        let sortedData = [...data];
-        if (selected === "추천순") {
-            sortedData.sort((a, b) => {
-                return a.recommendation < b.recommendation ? 1 : -1;
-            });
-        } else if (selected === "브랜드명순") {
-            sortedData.sort((a, b) => {
-                return a.brand > b.brand ? 1 : -1;
-            });
-        } else if (selected === "낮은가격순") {
-            sortedData.sort((a, b) => {
-                return a.price > b.price ? 1 : -1;
-            });
-        } else {
-            sortedData.sort((a, b) => {
-                return a.price < b.price ? 1 : -1;
-            });
-        }
-        return sortedData;
-    }
+    const data = itemList;
 
-    const sortedChocoData = handleSortItems(selected);
+    // selected 상태값에 따른 초콜릿 데이터 정렬
+    const sortedChocoData = useMemo(() => {
+        let sortedData = [...data];
+
+        switch (selected) {
+            case "추천순":
+                sortedData.sort((a, b) => b.recommendation - a.recommendation);
+                break;
+            case "브랜드명순":
+                sortedData.sort((a, b) => a.brand.localeCompare(b.brand));
+                break;
+            case "낮은가격순":
+                sortedData.sort((a, b) => a.price - b.price);
+                break;
+            case "높은가격순":
+                sortedData.sort((a, b) => b.price - a.price);
+                break;
+        }
+
+        return sortedData;
+    }, [selected]);
 
     return (
         <>
@@ -54,9 +51,9 @@ export default function Content({ itemList, styles }: ContentProps) {
             />
             <div className={styles.item_container}>
                 <ul className={styles.item_li}>
-                    {sortedChocoData.map((chocolate, i) => {
+                    {sortedChocoData.map((chocolate) => {
                         return (
-                            <li className={styles.item} key={i}>
+                            <li className={styles.item} key={chocolate.id}>
                                 <ItemCard
                                     styles={styles}
                                     chocolate={chocolate}
