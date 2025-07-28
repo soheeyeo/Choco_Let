d16c1f9b8d178d1d7018ee902d9af71c72505869
soheeyeo <sohee407@gmail.com>
2025-07-28 20:25:41 +0900

    Implement custom error page in error.tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect } from "react";
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error;
+    reset: () => void;
+}) {
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+
+    return (
+        <div className="not_found_container">
+            <Image
+                src="/assets/emoji.png"
+                alt="not found"
+                width={300}
+                height={300}
+            />
+            <div className="not_found_content">
+                <p className="not_found_message">오류가 발생했습니다.</p>
+                <p className="not_found_message">잠시 후 다시 시도해주세요.</p>
+                <button className="not_found_btn" onClick={() => reset()}>
+                    다시 시도하기
+                </button>
+            </div>
+        </div>
+    );
+}
