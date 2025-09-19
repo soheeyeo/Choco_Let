503ee7d5e31a239ca0eb1cdcf076aec1a2d5ca0e
soheeyeo <sohee407@gmail.com>
2025-09-19 21:03:46 +0900

    ♻️ Migrate from NextAuth to Auth.js

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,9 +2,20 @@ import NextAuth from "next-auth";
 import { User as PrismaUser } from "@prisma/client";
 
 export declare module "next-auth" {
+    interface Session {
+        user: {
+            id: string;
+        } & DefaultSession["user"];
+    }
+
     interface User {
         id: string;
         email?: string;
         password?: string;
+    }
+
+    interface JWT {
+        id: string;
+        email?: string;
     }
 }
