99ce8c206904ac94d4847c2e257803c94b5c7c5e
soheeyeo <sohee407@gmail.com>
2025-10-15 22:53:21 +0900

    ♻️ Refactor authentication configuration to separate providers

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import authConfig from "./auth.config";
+import { authConfig } from "./auth.config";
 import NextAuth from "next-auth";
 
 const { auth } = NextAuth(authConfig);
